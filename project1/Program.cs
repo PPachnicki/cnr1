16810abfb959621f644f6b4b6e5271379ed1663e
Pawel Pachnikci <pachnicki1@yahoo.co.uk>
2021-10-18 16:36:16 +0100

    my first commint

---
+++ project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Are you OK today?");
+
+            string myday = Console.ReadLine();
+
+            if(myday == "Yes" || myday =="yes" || myday == "Yes" || myday == "YES" || myday =="y" || myday == "Y")
+            {
+                Console.WriteLine("Lovely!");
+            }
+            else{
+                Console.WriteLine("Grink some cofee than!");
+            }
+
+
+        }
+    }
+}
