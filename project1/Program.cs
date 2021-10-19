92a65f629243107d9ca0f9f282821d8152c4e8cf
Pawel Pachnikci <pachnicki1@yahoo.co.uk>
2021-10-19 18:05:44 +0100

    2nd commit

--- project1/Program.cs
+++ project1/Program.cs
@@ -7,19 +7,23 @@ namespace project1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string ans = Console.ReadLine();
+            Console.WriteLine($"Hello {ans}!");
 
             Console.WriteLine("Are you OK today?");
 
             string myday = Console.ReadLine();
 
-            if(myday == "Yes" || myday =="yes" || myday == "Yes" || myday == "YES" || myday =="y" || myday == "Y")
+            if(myday.ToLower().StartsWith("y")) //alternative to the line blow 
+            //if(myday == "Yes" || myday =="yes" || myday == "Yes" || myday == "YES" || myday =="y" || myday == "Y")
             {
                 Console.WriteLine("Lovely!");
             }
             else{
                 Console.WriteLine("Grink some cofee than!");
             }
-
+            /* random questions not completed */
 
         }
     }
